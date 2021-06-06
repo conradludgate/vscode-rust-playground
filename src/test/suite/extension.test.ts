06d5255d941cc147eb33f6df58e7ec209e1be323
Conrad Ludgate <conradludgate@gmail.com>
2021-06-06 16:56:17 +0100

    test: remove setup

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -6,11 +6,7 @@ import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-	after(async () => {
-		await vscode.commands.executeCommand("rust-playground.newPlayground");
-	});
-
-	test('Sample test', async () => {
+	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
